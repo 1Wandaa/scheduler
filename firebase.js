45c05d5f78387c556b6557780f40502244ed493a
1Wandaa <hydrich25@gmail.com>
2026-04-19 20:53:04 +0800

    Update configuration files and fix deployment bugs

--- firebase.js
+++ firebase.js
@@ -3,9 +3,9 @@ import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAN_6KthtgbGRNq6INNznVs73PH708f5OA",
-  authDomain: "scheduler-db2d5.firebaseapp.com",
-  projectId: "scheduler-db2d5",
-  storageBucket: "scheduler-db2d5.firebasestorage.app",
+  authDomain: "scheduler-app2503.firebaseapp.com",
+  projectId: "scheduler-app2503",
+  storageBucket: "scheduler-app2503.firebasestorage.app",
   messagingSenderId: "1049693325979",
   appId: "1:1049693325979:web:1cb643730fac9e4ae6943e"
 };
