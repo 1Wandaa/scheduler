8579183e2bf7d41279245d5263c5dfe25a3437e1
1Wandaa <hydrich25@gmail.com>
2026-06-16 13:05:49 +0800

    feat: add manifest.json and update application icons and metadata references

--- vite.config.js
+++ vite.config.js
@@ -20,13 +20,13 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/app-icon.png',
+            src: '/app-icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: '/app-icon.png',
+            src: '/app-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
