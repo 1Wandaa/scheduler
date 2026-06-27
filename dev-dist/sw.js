76b0442651d80257d53b595e167c24d2cb68e2da
1Wandaa <hydrich25@gmail.com>
2026-06-27 23:43:17 +0800

    feat: implement service worker caching and initialize global application styles

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gaseii7q3f8"
+    "revision": "0.riavgujg8ks"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
