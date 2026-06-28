1313d203a2515842be448ac0b73d94b7855a8412
1Wandaa <hydrich25@gmail.com>
2026-06-28 22:45:22 +0800

    feat: implement service worker for precaching and add no-cache header to firebase configuration

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mf27sa9hleg"
+    "revision": "0.lflkmf7loq4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
