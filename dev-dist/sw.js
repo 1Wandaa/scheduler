a60e543dc00c7b6324181f11a2a1f5d4a54322e1
1Wandaa <hydrich25@gmail.com>
2026-07-31 21:30:11 +0800

    feat: add comprehensive CSS styling and animation libraries for scheduler application components

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q11m8lm5em8"
+    "revision": "0.schr8m2kbjs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
